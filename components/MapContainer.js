3e4a6b1b84e49fc5d85a58ca0ffad9c1c367970f
jraz35fk <jrasze@gmail.com>
2025-02-10 22:31:07 -0500

    Update MapContainer.js

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -1,1 +1,14 @@
+// components/MapContainer.js
+import React from "react";
 
+function MapContainer({ placeLat, placeLon, zoom }) {
+  return (
+    <div style={{ width: "100%", height: "100%", backgroundColor: "#ddd" }}>
+      {/* Replace this with your actual map library code */}
+      <p>Map Placeholder</p>
+      <p>Lat: {placeLat}, Lon: {placeLon}, Zoom: {zoom}</p>
+    </div>
+  );
+}
+
+export default MapContainer;
