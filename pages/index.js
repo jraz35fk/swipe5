d3eec0640caa7c37b194785c018eed98c48c059f
jraz35fk <jrasze@gmail.com>
2025-02-13 16:16:24 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Welcome to Swipe5</h1>
+            <p>Your app has deployed successfully!</p>
+        </div>
+    );
+}
